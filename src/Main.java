73dbcbcf7950428ea6c621ad20d9ca00ade50665
20080201 <1847623118@qq.com>
2026-03-15 17:58:51 +0800

    feat:完成第二个Java程序

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "奈良";
+        int age = 18;
+        System.out.println("第1次尝试");
+        System.out.println("我是" + name +"今年" + age + "岁");
+        String love = "哲学";
+        int  loveage = 5;
+        System.out.println("我喜欢哲学已经" + loveage + "年了");
+        System.out.println("初次尝试第一次纯文本");
+    }
+}
